e00327802acd5f7acc87abecace63e7b971e88ca
JayJay458 <qiujiajun0430@gmail.com>
2021-11-06 22:06:08 -0400

    Initial set up

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+        <div className="App">
+          Hello, World!
+        </div>
+    );
+  }
+}
+
+export default App;
