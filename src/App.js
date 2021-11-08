7b3cfd623c0015670b9559ef5918c339b80e6a86
JayJay458 <qiujiajun0430@gmail.com>
2021-11-07 20:01:24 -0500

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,31 +1,16 @@
-import React,{Component} from 'react';
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Routes,Route} from 'react-router-dom';
+
 import Home from './pages/Home';
 
 class App extends Component {
-  
-  
-  constructor()
-  {
-  super();
-    this.state={
-     accountBalance:14568.27
-    }
-  }
-
   render() {
-    const HomeComponent=()=>(<Home accountBalance={this.state.accountBalance}/>);
     return (
-      <Router> 
-        <Routes>
-                
-        
-          <Route exact path="/" render={HomeComponent}/>
-
-        </Routes>
-        
-      </Router>
-
+        <Router>
+          <Routes>
+            <Route exact path="/" component={Home}/>
+          </Routes>
+        </Router>
     );
   }
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,16 @@
-    // src/components/Home.js
+      // src/components/Home.js
     
-    import React, { Component } from 'react';
-    import AccountBalance from './AccountBalance';
-
-    class Home extends Component {
-      render() {
-        return (
-            <div>
-              <img src="https://www.reuters.com/resizer/R13b7gZ-xOpJZSviLQNCkRUIhD0=/960x0/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/C44YCSGHYJNU3FOAERYSMGIBWI.jpg" alt="bank"/>
-              
-              <h1>Bank of React</h1>  
-
-              <AccountBalance accountbalance={this.props.accountbalance}/>
-            
-            </div>
-        );
+      import React, { Component } from 'react';
+    
+      class Home extends Component {
+        render() {
+          return (
+              <div>
+                <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg" alt="bank"/>
+                <h1>Bank of React</h1>
+              </div>
+          );
+        }
       }
-    }
-    
-    export default Home;
+      
+      export default Home;
