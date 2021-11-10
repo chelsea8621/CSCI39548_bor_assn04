f7d8157a9859666d4ba6010bdc93d1a4e8bf9df7
JayJay458 <qiujiajun0430@gmail.com>
2021-11-09 23:38:52 -0500

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,100 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Routes,Route} from 'react-router-dom';
-
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './pages/Home';
+import UserProfile from './pages/UserProfile';
+import Login from './pages/Login';
+import axios from "axios"
+import './App.css'
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      accountBalance: 0,
+      currentUser:{
+        userName: 'joe_shmo',
+        memberSince: '07/23/96'
+      },
+      debits: [],
+      credits:[],
+    }
+  }    
+
+  async componentDidMount()
+  {
+    let debits=await axios.get("https://moj-api.herokuapp.com/debits")
+    let credits=await axios.get("https://moj-api.herokuapp.com/credits")
+
+    debits=debits.data
+    credits=credits.data
+
+    var creditSum=0;
+    var debitSum=0;
+    debits.forEach((debit)=>{
+      debitSum+=debit.amount
+    })
+    credits.forEach((credit)=>{
+      creditSum+=credit.amount
+    })
+
+    let accountBalance=creditSum-debitSum;
+    this.setState({debits,credits,accountBalance});
+    console.log(debits);
+  }
+
+  refreshBalance=()=>{
+    var credits=this.state.credits;
+    var debits=this.state.debits;
+
+    var creditsTotal=0;
+    var debitsTotal=0;
+    credits.forEach((credit)=>{
+      creditsTotal+=credit;
+    })
+    debits.forEach((debit)=>{
+      debitsTotal+=debit;
+    })
+    this.setState({accountBalance:creditsTotal-debitsTotal});
+  }
+  addCredit=(credit)=>{
+    this.setState({credits:this.state.credits.concat(credit)});
+    this.refreshBalance();
+  }
+
+  addDebit=(debit)=>{
+    this.setState({debits:this.state.debits.concat(debit)});
+    this.refreshBalance();
+  }
+
+  mockLogIn = (logInInfo) => {
+      const newUser = {...this.state.currentUser}
+      newUser.userName = logInInfo.userName
+      this.setState({currentUser: newUser})
+  }
+
+  
   render() {
+
+    const HomeComponent = () => (<Home accountBalance={this.state.accountBalance}/>);
+    const UserProfileComponent = () => (
+        <UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince}  />
+    
+    );
+    const LogInComponent = () => (<Login user={this.state.currentUser} mockLogIn={this.mockLogIn} />)
+    
+
+
     return (
         <Router>
-          <Routes>
-            <Route exact path="/" component={Home}/>
-          </Routes>
+          <div>
+            <Route exact path="/" render={HomeComponent}/>
+            <Route exact path="/userProfile" render={UserProfileComponent}/>
+            <Route exact path="/login" render={LogInComponent}/>
+          </div>
         </Router>
     );
   }
 }
 
-export default App;
+export default App
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,22 @@
-      // src/components/Home.js
-    
-      import React, { Component } from 'react';
-    
-      class Home extends Component {
-        render() {
-          return (
-              <div>
-                <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg" alt="bank"/>
-                <h1>Bank of React</h1>
-              </div>
-          );
-        }
-      }
-      
-      export default Home;
+// src/components/Home.js
+
+import React, {Component} from 'react';
+import AccountBalance from './AccountBalance';
+import {Link} from 'react-router-dom';
+
+class Home extends Component {
+  render() {
+    return (
+        <div>
+          <img src="https://picsum.photos/201" alt="bank"/>
+          <h1>Bank of React</h1>
+
+          <Link to="/userProfile">User Profile</Link>
+          <Link to="/Login">Login </Link>
+          <AccountBalance accountBalance={this.props.accountBalance}/>
+        </div>
+    );
+  }
+}
+
+export default Home;
