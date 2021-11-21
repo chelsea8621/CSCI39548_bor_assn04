1713b8563a3e97a040c3c74a9bf212c9a2c189d2
JayJay458 <qiujiajun0430@gmail.com>
2021-11-21 16:16:29 -0500

    initial css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,19 +2,21 @@
 
 import React, {Component} from 'react';
 import AccountBalance from './AccountBalance';
+import './style.css'
 import {Link} from 'react-router-dom';
 
 class Home extends Component {
   render() {
     return (
-        <div>
+        <div className='background'>
           <img src="https://picsum.photos/201" alt="bank"/>
-          <h1>Bank of React</h1>
-
-          <Link to="/userProfile">User Profile</Link> <br/>
-          <Link to="/Login">Login </Link> <br/>
-          <Link to="/Debits">Debits </Link> <br/> <br/>
-          <Link to="/Credits">Credits </Link> <br/> <br/>
+          <h1 className='title'>Bank of React</h1>
+          <div className='link'>
+            <Link  to="/userProfile">User Profile</Link> <br/>
+            <Link to="/Login">Login </Link> <br/>
+            <Link to="/Debits">Debits </Link> <br/> <br/>
+            <Link to="/Credits">Credits </Link> <br/> <br/>
+          </div>
           <AccountBalance accountBalance={this.props.accountBalance}/>
         </div>
     );
